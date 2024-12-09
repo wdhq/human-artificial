f079c03ba136f94c1464a4f2f7ca5f0871f16ea6
wdhq <160621588+wdhq@users.noreply.github.com>
2024-12-09 17:28:39 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Fetch and insert the header
-fetch('/components/header.html')
+fetch('components/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;
